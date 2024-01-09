623373473f35a1397460c18fe7861d42de44438e
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-09 15:17:26 +0100

    create show

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.lessons.java.springlamiapizzeriacrud.controller;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.java.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizza")
@@ -23,6 +27,19 @@ public class PizzaController {
         model.addAttribute("pizzaList", pizzaList);
 
         return "pizza/list";
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if (result.isPresent()) {
+            Pizza pizza = result.get();
+            model.addAttribute("pizza", pizza);
+            return "pizza/show";
+        } else {
+            // gestisco il caso in cui nel database un Book con quell'id non c'è
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
     }
 
 }
