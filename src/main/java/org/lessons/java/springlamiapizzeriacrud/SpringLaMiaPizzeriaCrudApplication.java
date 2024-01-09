3e2cf3dbbaea3871636659f7dfba86e93ad3484d
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-09 11:00:15 +0100

    initial commit

---
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.springlamiapizzeriacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/springlamiapizzeriacrud/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.springlamiapizzeriacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
