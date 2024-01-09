d64690378857fae32cc49da558c85a9589dacbe0
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-09 11:42:13 +0100

    create repository

---
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -0,0 +1,9 @@
+package org.lessons.java.springlamiapizzeriacrud.repository;
+
+import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// interfaccia che eredita da JpaRepository tutti i metodi che permettono di fare le CRUD
+// i generics chiedono: che tipo di dato è l'entità (Pizza) e che tipo di dato è la chiave primaria (Integer)
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+}
